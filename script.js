67f5d7a2082457dfc1abff05ae26c403b2f6e50b
zalec <142818531+z4le3c@users.noreply.github.com>
2023-09-27 10:49:16 -0600

    feat: validation added

---
+++ script.js
@@ -0,0 +1,28 @@
+const submitButton = document.querySelector('.button-section button');
+const inputs = document.querySelectorAll('input');
+const passwI1 = document.querySelector('#password');
+const passwI2 = document.querySelector('#confirm-passw');
+submitButton.addEventListener('click', () => {
+    let passw1 = passwI1.value;
+    let passw2 = passwI2.value;
+    if (passw1 == passw2) {
+        passwI1.classList.remove('invalid')
+        passwI2.classList.remove('invalid')
+    }
+
+    for (const input of inputs) {
+        if (input.validity.valid) {
+            input.classList.remove('invalid');
+        } else {
+            input.classList.add('invalid');
+        }
+    }
+
+    if (passw1 != passw2) { 
+        passwI1.setCustomValidity("Passwords do not match")
+        passwI1.classList.add('invalid')
+        passwI2.classList.add('invalid')
+    } else {
+        passwI1.setCustomValidity("")
+    }
+});
